069bbd094220c024e1f74e10b59b37e9911acde1
chrisver21 <chrisvergara009@gmail.com>
2025-05-15 15:01:49 +0800

    initial commit.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,33 @@
 import { Button } from "@/components/ui/button";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-     <h1 className="text-4xl font-bold">PDF Quiz Generator</h1>
+      <h1 className="text-4xl font-bold">PDF Quiz Generator</h1>
+
+      <ResizablePanelGroup
+        direction="vertical"
+        className="min-h-[200px] max-w-md rounded-lg border md:min-w-[450px]"
+      >
+        <ResizablePanel
+          defaultSize={25}
+          className="flex h-full items-center justify-center p-6"
+        >
+          <Button>Upload PDF</Button>
+        </ResizablePanel>
+        <ResizableHandle />
+        <ResizablePanel
+          defaultSize={75}
+          className="flex h-full items-center justify-center p-6"
+        >
+          <Button>Generate sample Test</Button>
+        </ResizablePanel>
+      </ResizablePanelGroup>
     </div>
   );
 }
